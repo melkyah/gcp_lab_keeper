4118ff17c3fd5a4d511aa80526aa356529327103
Francisco Gonzalez Ratti <franciscog.mk@gmail.com>
2019-09-13 13:00:20 -0300

    Added handling of desired zones and recursive calls.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+"""This module gets a list of instances running in a given GCP project and shuts them down."""
+
 # Copyright 2018 GCP Instance Watcher authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +15,7 @@
 # limitations under the License.
 
 from pprint import pprint
+import re
 
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
@@ -22,17 +25,62 @@ CREDENTIALS = GoogleCredentials.get_application_default()
 SERVICE = discovery.build('compute', 'v1', credentials=CREDENTIALS)
 
 # Project ID for this request.
-PROJECT = 'prod-host-4f86'  # TODO: Update placeholder value.
+PROJECT = 'prod-host-4f86'  # TODO: Make this an input parameter.
 
-# The name of the zone for this request.
-ZONE = 'us-west1-b'  # TODO: Update placeholder value.
+# The list of the zone prefixes for this request.
+WANTED_ZONES_PREFIXES = ['us']  # TODO: Make this an input parameter.
 
-REQUEST = SERVICE.instances().list(project=PROJECT, zone=ZONE)
-while REQUEST is not None:
-    RESPONSE = REQUEST.execute()
+def request_instances(project, zone):
+    """Make an API call to return all existing instances for a zone in a project."""
 
-    for instance in RESPONSE['items']:
-        # TODO: Change code below to process each `instance` resource:
-        pprint(instance)
+    instance_request = SERVICE.instances().list(project=project, zone=zone)
 
-    REQUEST = SERVICE.instances().list_next(previous_request=REQUEST, previous_response=RESPONSE)
+    while instance_request is not None:
+        response = instance_request.execute()
+
+        if 'items' in response:
+            for instance in response['items']:
+                # TODO: Change code below to process each `instance` resource:
+
+                pprint(instance)
+
+            instance_request = SERVICE.instances().list_next(previous_request=instance_request,
+                                                             previous_response=response)
+        return # TODO: This function still doesn't return anything
+
+def request_zones(project):
+    """Make an API call to return all available zones for a project."""
+    zone_request = SERVICE.zones().list(project=project)
+    while zone_request is not None:
+        response = zone_request.execute()
+        zone_list = []
+        for zone in response['items']:
+            # Puts all zone names inside a list.
+            zone_list.append(zone.get('name', None))
+
+        zone_request = SERVICE.zones().list_next(previous_request=zone_request,
+                                                 previous_response=response)
+
+        return zone_list
+
+def filter_zones(wanted_zones_prefix, zone_list):
+    """Filter unwanted zones from a zone list using a prefix."""
+    target_zones = []
+
+    for zone in zone_list:
+        for prefix in wanted_zones_prefix:
+            pattern = re.compile(f'^{prefix}')
+            if pattern.match(zone):
+                target_zones.append(zone)
+
+    return target_zones
+
+# Get a list of zone names
+ZONE_LIST = request_zones(PROJECT)
+
+# Get only wanted zones
+TARGET_ZONES = filter_zones(WANTED_ZONES_PREFIXES, ZONE_LIST)
+
+# Get a list of instances for each target zone
+for zone in TARGET_ZONES:
+    request_instances(PROJECT, zone)
