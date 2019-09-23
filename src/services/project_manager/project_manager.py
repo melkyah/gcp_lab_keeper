ac1b2159f10f2e9ec7264bac5c2e63241ed60215
Francisco Gonzalez Ratti <franciscog.mk@gmail.com>
2019-09-23 12:01:19 -0300

    Added server startup for Project Manager

--- src/services/project_manager/project_manager.py
+++ src/services/project_manager/project_manager.py
@@ -14,3 +14,47 @@
 """
 This module manages Google Cloud Projects.
 """
+
+import time
+from pprint import pprint
+from concurrent import futures
+from googleapiclient import discovery
+
+import grpc
+import project_manager_pb2
+import project_manager_pb2_grpc
+
+from lib.lab_keeper.config import Config
+from lib.lab_keeper.authenticator import Authenticator
+
+class ProjectManagerServicer(project_manager_pb2_grpc.ProjectManagerServicer):
+    """gRPC server for Project Manager service."""
+    def __init__(self):
+        self.config = Config()
+        self.authenticator = Authenticator()
+        self.server_port = self.config.project_manager_port
+
+    def start_server(self):
+        """
+        Start the gRPC server, and prep
+        it for serving incoming connections
+        """
+        project_manager_server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=10))
+        project_manager_pb2_grpc.add_ProjectManagerServicer_to_server(
+            ProjectManagerServicer(), project_manager_server)
+        project_manager_server.add_insecure_port(
+            f'[::]:{self.server_port}')
+        project_manager_server.start()
+
+        pprint(f"Project Manager Server running in port {self.server_port}...")
+
+        try:
+            while True:
+                time.sleep(60*60*60)
+        except KeyboardInterrupt:
+            project_manager_server.stop(0)
+            pprint("Instance Manager Server stopped...")
+
+curr_server = ProjectManagerServicer()
+curr_server.start_server()
