45e5ea36cc35d25ad8f550b28279edf7857310f4
Francisco Gonzalez Ratti <franciscog.mk@gmail.com>
2019-09-23 18:00:06 -0300

    Cambios en frontend

--- src/services/frontend/src/components/ProjectSelector/ProjectSelector.vue
+++ src/services/frontend/src/components/ProjectSelector/ProjectSelector.vue
@@ -18,10 +18,44 @@
 </template>
 
 <script>
+
+import { Credentials, GetProjectRequest } from "./project_manager_pb";
+import { ProjectManagerClient } from "./project_manager_grpc_web_pb";
+import {grpc, BrowserHeaders} from "grpc-web-client";
+
 export default {
   name: "ProjectSelector",
   props: {
     msg: String
+  },
+  data: function(){
+      return {
+          projectManagerHost: "localhost",
+          projectManagerPort: "8090",
+          credPath: "../../assets/credentials.json",
+          projectList: [],
+          client: null
+      };
+  },
+  created: function(){
+      this.client = new ProjectManagerClient(
+          `http://${this.projectManagerHost}:${this.projectManagerPort}`, null, null
+          );
+      this.getProjects(this.credPath);
+  },
+  // Start method definitions
+  methods: {
+      getProjects: function(credFile){
+          let credentials = new Credentials();
+          let request = new GetProjectRequest();
+          credentials.setCredentials(credFile);
+          request.setCredentials = credentials;
+        
+          this.client.getProjects(request, function(error, result) {
+            if (error) console.log('Error: ', error);
+            else console.log(result.toObject());
+          });
+      },
   }
-};
+}
 </script>
