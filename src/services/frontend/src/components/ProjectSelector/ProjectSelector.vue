bbb55e6a72a2a4fb1eaede29531ad600203e5c08
Francisco Gonzalez <franciscog.mk@gmail.com>
2019-09-24 12:37:31 -0300

    Project list appears correctly in dropdown selector

--- src/services/frontend/src/components/ProjectSelector/ProjectSelector.vue
+++ src/services/frontend/src/components/ProjectSelector/ProjectSelector.vue
@@ -3,12 +3,9 @@
     <form>
       <div class="form-row">
         <label for="projectSelector" class="col-sm-4 col-form-label">Project:</label>
-        <select class="form-control col-sm-6" id="projectSelector">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <select v-model="selectedProject" class="form-control col-sm-6" id="projectSelector">
+          <option value selected disabled>Select project...</option>
+          <option v-for="(item, index) in projectList" v-bind:key="index">{{item.name}}</option>
         </select>
       </div>
     </form>
@@ -32,7 +29,8 @@ export default {
       projectList: null,
       client: null,
       request: null,
-      credentials: null
+      credentials: null,
+      selectedProject: ""
     };
   },
   created: function() {
